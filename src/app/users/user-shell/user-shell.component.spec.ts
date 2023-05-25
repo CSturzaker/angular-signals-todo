10c9c55af93c4150aa6f3777f4c04d032ca1cf36
CSturzaker <craig.sturzaker@arup.com>
2023-05-25 12:46:10 +0100

    feat(user shel): create shell ui component

---
+++ src/app/users/user-shell/user-shell.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserShellComponent } from './user-shell.component';
+
+describe('UserShellComponent', () => {
+  let component: UserShellComponent;
+  let fixture: ComponentFixture<UserShellComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [UserShellComponent]
+    });
+    fixture = TestBed.createComponent(UserShellComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
